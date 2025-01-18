f1c7302390967dd6129552fb54312a6be995a475
shubham kopare <149860270+shubhamkopare1@users.noreply.github.com>
2025-01-18 16:17:44 +0530

    remove uniqueness

--- models/contactModel.js
+++ models/contactModel.js
@@ -9,7 +9,7 @@ const contactSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    // unique: true,
+    
     match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
   },
   phone: {
